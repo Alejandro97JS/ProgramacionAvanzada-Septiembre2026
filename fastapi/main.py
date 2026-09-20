608516ab8bfb6401f0825b9ec2db1a9d45685e4b
davdoors <david.dorado.sevilla@gmail.com>
2026-09-20 13:18:15 +0200

    Uso de recursos estaticos en FastAPI

--- fastapi/main.py
+++ fastapi/main.py
@@ -1,11 +1,19 @@
 from fastapi import FastAPI
 from routers import products, users
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 
 # Añadir routers
 app.include_router(products.router)
 app.include_router(users.router)
+
+# Montar la carpeta con los archivos estáticos
+app.mount(
+    "/static", # path para exponer el directorio en la url
+    StaticFiles(directory="static"), # path de la carpeta en el proyecto
+    name="static" # alias para url_for. Si no se usa url_for en el proyecto, no se necesita
+)
 
 @app.get("/")
 async def root():
